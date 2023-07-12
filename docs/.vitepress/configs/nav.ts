91ffdfdb3cf65c6ecd4028778a54f396b0341006
轻虹空雨 <mufeng086@gmail.com>
2023-07-12 12:07:50 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -9,5 +9,9 @@ export const nav: DefaultTheme.Config['nav'] = [
   {
     text: '友情赞助',
     link: 'https://mufeng086.com/support'
+  },
+  {
+    text: '反馈通道',
+    link: 'https://docs.qq.com/form/page/DTlRZSU9RVE1YcEZW?u=3f2a61a5441245c7831c8b16bf0b34d4'
   }
 ]
